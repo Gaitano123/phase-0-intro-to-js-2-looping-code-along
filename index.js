4ba5b87028cbf779312d8e21aa1ccdc476c2c804
Gaitano <gaitano.george@student.moringaschool.com>
2023-06-11 22:01:57 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+writeCards(["Adda", "brendan", "Ali"], "suprise");
+function writeCards(names, event) {
+    let arrayOfThanks =[];
+    for (let i = 0; i < names.lenth; i++) {
+        arrayOfThanks.push('Thankyou, ${names[i]}, for the wonderful ${event} gift')
+    }
+    return arrayOfThanks;
+}
+function countDown() {
+    let counter = 10;
+    while (counter >=0) {
+        console.log(counter);
+        counter--;
+    }
+}
+
+countDown();
